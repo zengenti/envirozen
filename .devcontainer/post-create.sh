613314fabbb79a8809469210602bc08f54fcd202
Joe J Collins <joejcollins@users.noreply.github.com>
2023-10-19 14:47:29 +0000

    Add pyenv

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -3,5 +3,7 @@
 echo 'eval "$(starship init bash)"' >> ~/.bashrc
 # Then add pyenv
 curl https://pyenv.run | bash
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
 echo 'I be done now!'
