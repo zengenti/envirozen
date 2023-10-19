1b70a5089bdbddf9963dc183011a437c7ee3e47a
Joe J Collins <joejcollins@users.noreply.github.com>
2023-10-19 13:42:51 +0000

    Dockerfile

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Activate Starship in the .bashrc
-curl https://pyenv.run | bash
 echo 'eval "$(starship init bash)"' >> ~/.bashrc
 echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
+echo 'I be done now!'
